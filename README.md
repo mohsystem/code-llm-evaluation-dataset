8bd10c0d6b9895d735e6e91c37d057a7c02605c9
mohkharma <moh.kharma@gmail.com>
2024-06-11 02:18:06 +0300

    Implement PromptDescriptionValidator
    Provide Task No.1

---
+++ README.md
@@ -0,0 +1,14 @@
+## Introduction
+Dataset for evaluating LLMs generated code.
+
+## Getting Started
+To get started with the project, follow these steps:
+
+1. Clone the repository: `git clone https://github.com/your-username/your-project.git`
+2. Install dependencies:
+3. Run the project: 
+
+## Usage
+Here's a brief guide on how to use the project:
+
+
---
+++ src/main/java/edu/bzu/PromptDescriptionValidator.java
@@ -0,0 +1,47 @@
+package edu.bzu;
+
+import org.everit.json.schema.Schema;
+import org.everit.json.schema.loader.SchemaLoader;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class PromptDescriptionValidator {
+
+    public static void main(String[] args) {
+        InputStream jsonStream = null;
+        InputStream schemaStream = null;
+        try {
+            // Load the JSON schema
+            schemaStream = new FileInputStream("promptDescription/promptDescriptionSchema.json");
+            JSONObject rawSchema = new JSONObject(new JSONTokener(schemaStream));
+            Schema schema = SchemaLoader.load(rawSchema);
+
+            jsonStream = new FileInputStream("promptDescription/promptDescription.json");
+            JSONObject jsonToValidate = new JSONObject(new JSONTokener(jsonStream));
+
+            schema.validate(jsonToValidate);
+            System.out.println("JSON is valid against the schema.");
+        } catch (Exception e) {
+            System.out.println("JSON validation failed: " + e.getMessage());
+        } finally {
+            if (jsonStream != null) {
+                try {
+                    jsonStream.close();
+                } catch (IOException e) {
+                    e.getMessage();
+                }
+            }
+            if (schemaStream != null) {
+                try {
+                    schemaStream.close();
+                } catch (IOException e) {
+                    e.getMessage();
+                }
+            }
+        }
+    }
+}
