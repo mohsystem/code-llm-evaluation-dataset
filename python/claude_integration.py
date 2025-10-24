fbf08414244bc59edde27c21adea23c5d8b09ce5
Mohammad <moh.kharma@gmail.com>
2025-10-25 01:31:36 +0300

    Update claude_integration.py

--- python/claude_integration.py
+++ python/claude_integration.py
@@ -6,7 +6,7 @@ class ClaudeIntegration:
         # https://docs.anthropic.com/en/docs/about-claude/models
         # https://console.anthropic.com/settings/plans
         self.model = model_name
-        SESSION_KEY = "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"
+        SESSION_KEY = "sk-ant-api03-_kglkggOpsMs-iKtVi63PCEXvFFVLm6LtretgfcbjhINXylmQ-LljkljkldfA"
         self.client = anthropic.Anthropic(api_key=SESSION_KEY,)
 
     def generate_content(self, prompt_system, prompt_desc):
