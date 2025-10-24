52f4a90df506dd378dff20ef411a9909150caa11
Mohammad <moh.kharma@gmail.com>
2025-10-25 01:31:53 +0300

    Update API key in GeminiIntegration class

--- python/gemini_integration.py
+++ python/gemini_integration.py
@@ -6,7 +6,7 @@ class GeminiIntegration:
     def __init__(self, model_name):
         # https://console.cloud.google.com/vertex-ai/generative/language/create/text?createType=code&project=gen-lang-client-0375481745
         # https://cloud.google.com/vertex-ai/generative-ai/docs/learn/models?_gl=1*x4pqgs*_ga*MTYyMTczNjUwMC4xNzE5MzE0MjQz*_ga_WH2QY8WWF5*MTcyMDIyMDU0NC44LjEuMTcyMDIyMDU0NS41OS4wLjA.&_ga=2.122733579.-1621736500.1719314243
-        genai.configure(api_key='AIzaSyB54k37O691yozKJlOcE0bczSajg1F0Qw0')
+        genai.configure(api_key='CEXvFFVLm6LtretgfcbjhINXylmQ-Lljkl')
         from google.generativeai.types import generation_types
         conf= generation_types.GenerationConfigType
         conf.max_output_tokens=4096
